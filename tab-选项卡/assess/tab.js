f8613e5971951efdec6dc88b62468c9ece2255ff
liyuan <1009102873@qq.com>
2016-07-15 14:46:45 +0800

    选项卡切换插件

---
+++ tab-选项卡/assess/tab.js
@@ -0,0 +1,28 @@
+/*
+     * tab切换插件
+     * 用例：$('*').createTab();
+     */
+    $.fn.createTab = function (opt) {
+        var def = {
+            activeEvt: 'mouseover',
+            activeCls: 'cur'
+        };
+        $.extend(def, opt);
+        this.each(function () {
+            var $this = $(this);
+            var timer;
+            $this.find('ul.title li').mouseover(def.activeEvt,function(){
+                var index = $(this).index(),
+                    that = $(this);
+                timer = setTimeout(function(){
+                    that.addClass('cur').siblings().removeClass('cur');
+                    $this.find('div.list').animate({marginLeft:-1000*index},'slow');
+                },300);
+            }).mouseout(function(){
+                clearTimeout( timer );
+            })
+        });
+    }
+$(function(){
+    $(".jyTable").createTab()
+});
